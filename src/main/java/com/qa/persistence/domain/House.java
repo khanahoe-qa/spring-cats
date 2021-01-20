7974701b4bd072ea6123751e0d415b18fa20cf02
Kieran Hanahoe <kjhanahoe@hotmail.co.uk>
2021-01-20 11:24:54 +0000

    kh: addded house domain, dto, service, controller

--- src/main/java/com/qa/persistence/domain/House.java
+++ src/main/java/com/qa/persistence/domain/House.java
@@ -12,11 +12,18 @@ import javax.persistence.OneToMany;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
 public class House {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Exclude
 	private Long id;
 	
 	@OneToMany(mappedBy = "house")
@@ -24,15 +31,15 @@ public class House {
 	private List<Cat> cats = new ArrayList<>();
 	
 	private String name;
-
+	
 	public House(String name) {
+		super();
 		this.name = name;
 	}
 	
 	public House(Long id, String name) {
+		super();
 		this.id = id;
 		this.name = name;
 	}
-	
-	
 }
